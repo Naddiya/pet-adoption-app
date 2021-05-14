887dca3d56e62ace9b5337de6318fc63c7c03f40
Naddiya <nadiamedkouri@gmail.com>
2021-05-14 18:03:41 +0200

    feat: animal & breed selector

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import ReactDOM from 'react-dom';
-import Pet from './Pet';
+// import Pet from './Pet';
+import SearchParams from './SearchParams'
 
 const App = () => {
     return (
         <div>
             <h1>Adopt Me!</h1>
-            <Pet name="Nmichou" animal="Fox" breed="Fennec" />
-            <Pet name="Coco" animal="Parrot" breed="Gabonese" />
-            <Pet name="Piouf" animal="Cat" breed="Vagabond" />
+            <SearchParams />
         </div>
     );
 };
