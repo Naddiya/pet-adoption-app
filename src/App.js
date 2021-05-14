666ab139d9237c8ad25badf05cfe90ba236fe730
Naddiya <nadiamedkouri@gmail.com>
2021-05-14 11:51:22 +0200

    feat: behind curtain react App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h2", {}, "Tricky"),
-        React.createElement("h3", {}, "Dog"),
-        React.createElement("h3", {}, "Braque"),
+        React.createElement("h2", {}, props.name),
+        React.createElement("h3", {}, props.animal),
+        React.createElement("h3", {}, props.breed),
     ])
 }
 
@@ -11,10 +11,10 @@ const App = () => {
         "div",
         {},
         [
-            React.createElement("h1", {}, "Adopt me!"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
+            React.createElement("h1", {id: "my brand"}, "Adopt me!"),
+            React.createElement(Pet, {name: "Nmichou", animal: "Fox", breed:"Fennec"}),
+            React.createElement(Pet, {name: "Coco", animal: "Parrot", breed:"Gabonese" }),
+            React.createElement(Pet, {name: "Piouf", animal: "Cat", breed: "Vagabond"}),
         ]
     );
 };
