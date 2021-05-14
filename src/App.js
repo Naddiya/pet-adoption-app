33ce58d38db9625a3a55eef671ef463c40fd732e
Naddiya <nadiamedkouri@gmail.com>
2021-05-14 13:18:55 +0200

    pj: prettier

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ const Pet = (props) => {
         React.createElement("h2", {}, props.name),
         React.createElement("h3", {}, props.animal),
         React.createElement("h3", {}, props.breed),
-    ])
-}
+    ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {id: "my brand"}, "Adopt me!"),
-            React.createElement(Pet, {name: "Nmichou", animal: "Fox", breed:"Fennec"}),
-            React.createElement(Pet, {name: "Coco", animal: "Parrot", breed:"Gabonese" }),
-            React.createElement(Pet, {name: "Piouf", animal: "Cat", breed: "Vagabond"}),
-        ]
-    );
+    return React.createElement("div", {}, [
+        React.createElement("h1", { id: "my brand" }, "Adopt me!"),
+        React.createElement(Pet, {
+            name: "Nmichou",
+            animal: "Fox",
+            breed: "Fennec",
+        }),
+        React.createElement(Pet, {
+            name: "Coco",
+            animal: "Parrot",
+            breed: "Gabonese",
+        }),
+        React.createElement(Pet, {
+            name: "Piouf",
+            animal: "Cat",
+            breed: "Vagabond",
+        }),
+    ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
