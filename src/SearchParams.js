445bcde4131808526b0757e0d1f76a6a51e26a69
Naddiya <nadiamedkouri@gmail.com>
2021-05-14 20:42:38 +0200

    feat: api data

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Pet from './Pet';
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("Seattle, WA");
+    const [location, setLocation] = useState("");
     const [animal, setAnimal] = useState("");
     const [breed, setBreed] = useState("");
+    const [pets, setPets] = useState([]);
 
     const breeds = [];
+
+    useEffect(() => {
+        requestPets();
+    }, []);
+
+    useEffect(() => {
+        const timer = setTimeout(() => alert('hi'), 30000);
+
+        return () => clearTimeout(timer);
+    }, [animal]);
+
+    async function requestPets() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+        );
+        const json = await res.json();
+
+        console.log(json);
+
+        setPets(json.pets);
+    }
 
     function updateLocation(e) {
         setLocation(e.target.value);
@@ -59,6 +82,11 @@ const SearchParams = () => {
                 </label>
                 <button>Submit</button>
             </form>
+            {
+                pets.map(pet => (
+                    <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+                ))
+            }
         </div>
     );
 };
