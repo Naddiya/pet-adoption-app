58973c581b734393a1d4a19cfa5ccb6b33f9a156
Naddiya <nadiamedkouri@gmail.com>
2021-05-16 18:04:21 +0200

    feat: error boundaries

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
 
 class Details extends Component {
     state = { loading: true };
@@ -24,6 +25,7 @@ class Details extends Component {
             return <h2> Loading ...</h2>;
         }
         const { animal, breed, city, state, description, name, images } = this.state;
+               
         return (
             <div className="details">
                 <Carousel images={images} />
@@ -33,10 +35,17 @@ class Details extends Component {
                     <button>Adopt {name}</button>
                     <p>{description}</p>
                 </div>
-
             </div>
         );
     }
 };
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsWithErrorBoundary() {
+    return  (
+        <ErrorBoundary>
+            <DetailsWithRouter />
+        </ErrorBoundary>
+    )
+}
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class ErrorBoundaries extends Component {
+    state = {
+        hasError: false
+    };
+    static getDerivedStateFromError() {
+        return { hasError: true };
+    }
+    componentDidCatch(error, info) {
+        console.error("ErrorBounday caught an error ", error, info);
+    }
+    render() {
+        if (this.state.hasError) {
+            return (
+                <h2>
+                    This listing has an error. <Link to="/">Click here</Link> to go back to home page
+                </h2>
+            );
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundaries;
