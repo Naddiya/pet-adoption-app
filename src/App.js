cbf0c43026c0b5a58d2347e1225ee9d95720fe9f
Naddiya <nadiamedkouri@gmail.com>
2021-05-16 13:15:11 +0200

    feat: navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import SearchParams from './SearchParams';
 import Details from './Details';
 
 const App = () => {
     return (
         <div>
-            <h1>Adopt Me!</h1>
             <Router>
+                <header>
+                    <Link to="/">Adopt Me!</Link>
+                </header>
                 <Route path="/details/:id">
                     <Details />
                 </Route>
